a605e6b294e941d9278601c3af0330f0b802534e
m <lmiguelvargasf@gmail.com>
2016-10-06 08:05:52 -0500

    Implement functions to obtain position and orientation given a pose

--- src/controller.py
+++ src/controller.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 import rospy
+import tf
+from nav_msgs.msg import Odometry
+
+i = 0
+
+def get_position(pose):
+    return pose.pose.position
+
+def get_orientation(pose):
+    quaternion = (
+        pose.pose.orientation.x,
+        pose.pose.orientation.y,
+        pose.pose.orientation.z,
+        pose.pose.orientation.w
+        )
+    return tf.transformations.euler_from_quaternion(quaternion)
 
 def compute_control_actions(msg):
-    pass
+    global i
+    pose = msg.pose
+    
+    current_position = get_position(pose)
+    current_orientation = get_orientation(pose)
+
 
 if __name__ == '__main__':
     rospy.init_node('controller')
