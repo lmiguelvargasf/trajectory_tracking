3cf51cf597a3ffc29603108f7665014e3c5e30e8
m <lmiguelvargasf@gmail.com>
2016-10-06 05:55:17 -0500

    Implement service callback

--- src/trajectory_server.py
+++ src/trajectory_server.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from trajectory_tracking.srv import TrajectoryPoint, TrajectoryPointResponse
+from geometry_msgs.msg import Point
 
-def compute_trajectory(request):
-    pass
+def compute_position(request):
+    position = Point()
+    position.x = 0.0
+    position.y = 1.0
+    position.z = 2.0
+    return TrajectoryPointResponse('position', position)
 
 
 if __name__ == '__main__':
     rospy.init_node('trajectory_server')
-    service = rospy.Service('trajectory', TrajectoryPoint, compute_trajectory)
+    service = rospy.Service('trajectory', TrajectoryPoint, compute_position)
     rospy.spin()
