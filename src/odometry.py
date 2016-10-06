ca5f604a91c43d24cb240b17151aaa54276c5c6f
m <lmiguelvargasf@gmail.com>
2016-10-06 03:38:02 -0500

    Subscribe to topic odom with callback get_odometry

--- src/odometry.py
+++ src/odometry.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import rospy
+from nav_msgs.msg import Odometry
+
+current_odometry = None
+
+def get_odometry(message):
+    global current_odometry
+    current_odometry = message
+
 
 if __name__ == '__main__':
     rospy.init_node('odometry')
-    rate = rospy.Rate(10)
+    subscriber = rospy.Subscriber('odom', Odometry, get_odometry)
     
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rate.sleep()
