017ab6ca120086e44d73d8fc666ac64d7649d51e
m <lmiguelvargasf@gmail.com>
2016-10-06 07:11:09 -0500

    Subscribe node to odometry_10_hz

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import rospy
 
+def compute_control_actions(msg):
+    pass
+
 if __name__ == '__main__':
     rospy.init_node('controller')
+    subscriber = rospy.Subscriber('odometry_10_hz', Odometry, compute_control_actions)
     rospy.spin()
