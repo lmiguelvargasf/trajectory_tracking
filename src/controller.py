41787abc7e350fa417d334a8456073f24d4ad49b
m <lmiguelvargasf@gmail.com>
2016-10-06 09:55:29 -0500

    Compute current and next references

--- src/controller.py
+++ src/controller.py
@@ -3,8 +3,10 @@ import rospy
 import tf
 from geometry_msgs.msg import Pose
 from trajectory_tracking.srv import TrajectoryPoint
+from constants import delta_t
 
 i = 0
+
 
 def get_position(pose):
     return pose.position
@@ -23,9 +25,14 @@ def compute_control_actions(pose):
     
     current_position = get_position(pose)
     current_orientation = get_orientation(pose)
+    service_proxy = rospy.ServiceProxy('trajectory', TrajectoryPoint)
+    
+    current_reference = service_proxy(i * delta_t)
+    next_reference = service_proxy((i + 1) * delta_t)
 
 
 if __name__ == '__main__':
     rospy.init_node('controller')
     subscriber = rospy.Subscriber('pose_10_hz', Pose, compute_control_actions)
+    rospy.wait_for_service('trajectory_server')
     rospy.spin()
