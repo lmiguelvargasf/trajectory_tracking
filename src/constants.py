78ba73998168d8e723d1c62942b19dabfd9ab229
m <lmiguelvargasf@gmail.com>
2016-10-09 17:37:01 -0500

    Define simulation time for linear and circular trajectories

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-SIMULATION_TIME_IN_SECONDS = 40
+TRAJECTORY_TYPE = 'circular'
+
+if TRAJECTORY_TYPE == 'linear':
+    SIMULATION_TIME_IN_SECONDS = 40
+elif TRAJECTORY_TYPE == 'circular':
+    SIMULATION_TIME_IN_SECONDS = 120
+
 DELTA_T = 0.1 # this is the sampling time
 STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
 K_V = 0.90
 K_W = 0.90
-TRAJECTORY_TYPE = 'linear'
