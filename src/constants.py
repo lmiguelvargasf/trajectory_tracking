468468369b4ce3058f0958169d8a1df04a1655a6
m <lmiguelvargasf@gmail.com>
2016-10-09 00:21:45 -0500

    Describe constant

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-DELTA_T = 0.1
 SIMULATION_TIME_IN_SECONDS = 40
+DELTA_T = 0.1 # this is the sampling time
 STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
 K_V = 0.90
 K_W = 0.90
