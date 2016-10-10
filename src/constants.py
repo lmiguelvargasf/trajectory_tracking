edadd7385ff8c839b524a6c06d0fc370c3db25bb
m <lmiguelvargasf@gmail.com>
2016-10-09 19:13:54 -0500

    Add simulation time for a squared trajectory

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-TRAJECTORY_TYPE = 'circular'
+TRAJECTORY_TYPE = 'squared'
 
 if TRAJECTORY_TYPE == 'linear':
-    SIMULATION_TIME_IN_SECONDS = 40
+    SIMULATION_TIME_IN_SECONDS = 40.0
 elif TRAJECTORY_TYPE == 'circular':
-    SIMULATION_TIME_IN_SECONDS = 120
+    SIMULATION_TIME_IN_SECONDS = 120.0
+elif TRAJECTORY_TYPE == 'squared':
+    SIMULATION_TIME_IN_SECONDS = 160.0
 
 DELTA_T = 0.1 # this is the sampling time
 STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
