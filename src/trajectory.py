61d234b9b625396814bc1d902d600f8626ae9f14
m <lmiguelvargasf@gmail.com>
2016-10-09 19:14:47 -0500

    Implement SquaredTrajectory class

--- src/trajectory.py
+++ src/trajectory.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import math
+
 from geometry_msgs.msg import Point
+
+from constants import SIMULATION_TIME_IN_SECONDS
 
 
 def create_trajectory(trajectory_type):
@@ -8,6 +11,8 @@ def create_trajectory(trajectory_type):
         return LinearTrajectory(0.05, 0, 0.05, 0)
     elif trajectory_type == 'circular':
         return CircularTrajectory(2.0, 120)
+    elif trajectory_type == 'squared':
+        return SquaredTrajectory(2.0, 0.25, 0.25)
 
 
 class Trajectory:
@@ -38,5 +43,31 @@ class CircularTrajectory(Trajectory):
         position = Point()
         position.x = self.radius * math.cos(2 * math.pi * t / self.period)
         position.y = self.radius * math.sin(2 * math.pi * t / self.period)
+
+        return position
+
+
+class SquaredTrajectory(Trajectory):
+    def __init__(self, side, x_0, y_0):
+        self.side = side
+        self.v = 4.0 * side / SIMULATION_TIME_IN_SECONDS
+        self.x_0 = x_0
+        self.y_0 = y_0
+
+    def get_position_at(self, t):
+        position = Point()
+
+        if 0 <= t < SIMULATION_TIME_IN_SECONDS / 4:
+            position.x = self.v * t + self.x_0
+            position.y = self.y_0
+        elif SIMULATION_TIME_IN_SECONDS / 4 <= t < SIMULATION_TIME_IN_SECONDS / 2:
+            position.x = self.x_0 + self.side
+            position.y = self.v * (t - SIMULATION_TIME_IN_SECONDS / 4) + self.y_0
+        elif SIMULATION_TIME_IN_SECONDS / 2 <= t < 3 * SIMULATION_TIME_IN_SECONDS / 4:
+            position.x = -self.v * (t - SIMULATION_TIME_IN_SECONDS / 2) + self.x_0 + self.side
+            position.y = self.y_0 + self.side
+        else:
+            position.x = self.x_0
+            position.y = -self.v * (t - 3 * SIMULATION_TIME_IN_SECONDS / 4) + self.y_0 + self.side
 
         return position
