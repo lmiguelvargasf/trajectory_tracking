f7fee1f2e1a67fad8460357044d4d56c1ae402c4
m <lmiguelvargasf@gmail.com>
2016-10-13 11:05:43 -0500

    Rename plots to plots_part_0

--- src/plotter.py
+++ src/plotter.py
@@ -13,7 +13,7 @@ class Plotter:
         self.y_ref = [trajectory.get_position_at(i * DELTA_T).y for i in range(STEPS)]
         self.x = []
         self.y = []
-        self.fig, self.plots = plt.subplots(2, 2, sharex=True)
+        self.fig_part_0, self.plots_part_0 = plt.subplots(2, 2, sharex=True)
 
     def add_point(self, pose):
         self.x.append(pose.position.x)
@@ -30,17 +30,18 @@ class Plotter:
         zeros = [0 for _ in range(STEPS)]
         e_x = [(a_i - b_i) for a_i , b_i in zip(self.x, self.x_ref)]
         e_y = [(a_i - b_i) for a_i , b_i in zip(self.y, self.y_ref)]
-        self.plots[0, 0].plot(self.t, self.x_ref, 'r--', label='ref')
-        self.plots[0, 0].plot(self.t, self.x, 'b', label='real')
-        self.plots[0, 1].plot(self.t, zeros, 'r--', label='e=0')
-        self.plots[0, 1].plot(self.t, e_x, 'b', label='x error')
-        self.plots[1, 0].plot(self.t, self.y_ref, 'r--', label='ref')
-        self.plots[1, 0].plot(self.t, self.y, 'b', label='real')
-        self.plots[1, 1].plot(self.t, zeros, 'r--', label='e=0')
-        self.plots[1, 1].plot(self.t, e_y, 'b', label='y error')
+        self.plots_part_0[0, 0].plot(self.t, self.x_ref, 'r--', label='ref', lw=3)
+        self.plots_part_0[0, 0].plot(self.t, self.x, 'b', label='real')
+        self.plots_part_0[0, 1].plot(self.t, zeros, 'r--', label='e=0', lw=2)
+        self.plots_part_0[0, 1].plot(self.t, e_x, 'b', label='x error')
+        self.plots_part_0[1, 0].plot(self.t, self.y_ref, 'r--', label='ref', lw=2)
+        self.plots_part_0[1, 0].plot(self.t, self.y, 'b', label='real')
+        self.plots_part_0[1, 1].plot(self.t, zeros, 'r--', label='e=0', lw=2)
+        self.plots_part_0[1, 1].plot(self.t, e_y, 'b', label='y error')
 
-        self.decorate_plot(self.plots[0, 0], 'x and x ref vs. t', 't[s]', 'x[m]')
-        self.decorate_plot(self.plots[0, 1], 'x error vs. t', 't[s]', 'x-error[m]')
-        self.decorate_plot(self.plots[1, 0], 'y and y ref vs. t', 't[s]', 'y[m]')
-        self.decorate_plot(self.plots[1, 1], 'y error vs. t', 't[s]', 'y-error[m]')
+        self.decorate_plot(self.plots_part_0[0, 0], 'x and x ref vs. t', 't[s]', 'x[m]')
+        self.decorate_plot(self.plots_part_0[0, 1], 'x error vs. t', 't[s]', 'x-error[m]')
+        self.decorate_plot(self.plots_part_0[1, 0], 'y and y ref vs. t', 't[s]', 'y[m]')
+        self.decorate_plot(self.plots_part_0[1, 1], 'y error vs. t', 't[s]', 'y-error[m]')
+
         plt.show()
