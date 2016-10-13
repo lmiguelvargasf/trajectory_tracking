753ff83b3a96579abf65a69b835c41ab1f0ed2ba
m <lmiguelvargasf@gmail.com>
2016-10-13 02:09:19 -0500

    Refactor: extract methods

--- src/controller.py
+++ src/controller.py
@@ -114,40 +114,55 @@ class PIDController(Controller):
         w_ref_n = self.compute_angular_speed_reference()
         w_ref_n = self.limit_angular_speed_reference(w_ref_n)
 
-        self.e_v_n = v_ref_n - v
-        self.e_w_n = w_ref_n - w
-        self.e_w_n = atan2(sin(self.e_w_n), cos(self.e_w_n))
+        self.e_v_n, self.e_w_n = self.compute_errors(v, v_ref_n, w, w_ref_n)
 
-        a_v = self.K_P_V + self.K_I_V * DELTA_T / 2 + self.K_D_V / DELTA_T
-        b_v = -self.K_P_V + self.K_I_V * DELTA_T / 2 - 2 * self.K_D_V / DELTA_T
-        c_v = self.K_D_V / DELTA_T
-
-        a_w = self.K_P_W + self.K_I_W * DELTA_T / 2 + self.K_D_W / DELTA_T
-        b_w = - self.K_P_W + self.K_I_W * DELTA_T / 2 - 2 * self.K_D_W / DELTA_T
-        c_w = self.K_D_W / DELTA_T
+        a_v, b_v, c_v = self.compute_v_pid_factors()
+        a_w, b_w, c_w = self.compute_w_pid_factors()
 
         self.v_c_n = self.v_c_nm1 + a_v * self.e_v_n + b_v * self.e_v_nm1 + c_v * self.e_v_nm2
         self.w_c_n = self.w_c_nm1 + a_w * self.e_w_n + b_w * self.e_w_nm1 + c_w * self.e_w_nm2
 
+        self.limit_linear_speed_control_action()
+        self.limit_angular_speed_control_action()
+        self.store_values_for_next_iteration()
 
-        if self.v_c_n > self.MAX_V:
-            self.v_c_n = self.MAX_V
-        elif self.v_c_n < -self.MAX_V:
-            self.v_c_n = -self.MAX_V
+    def store_values_for_next_iteration(self):
+        self.v_c_nm1 = self.v_c_n
+        self.w_c_nm1 = self.w_c_n
+        self.e_v_nm2 = self.e_v_nm1
+        self.e_w_nm2 = self.e_w_nm1
+        self.e_v_nm1 = self.e_v_n
+        self.e_w_nm1 = self.e_w_n
 
+    def limit_angular_speed_control_action(self):
         if self.w_c_n > self.MAX_W:
             self.w_c_n = self.MAX_W
         elif self.w_c_n < -self.MAX_W:
             self.w_c_n = -self.MAX_W
 
-        self.v_c_nm1 = self.v_c_n
-        self.w_c_nm1 = self.w_c_n
+    def limit_linear_speed_control_action(self):
+        if self.v_c_n > self.MAX_V:
+            self.v_c_n = self.MAX_V
+        elif self.v_c_n < -self.MAX_V:
+            self.v_c_n = -self.MAX_V
 
-        self.e_v_nm2 = self.e_v_nm1
-        self.e_w_nm2 = self.e_w_nm1
+    def compute_errors(self, v, v_ref_n, w, w_ref_n):
+        e_v = v_ref_n - v
+        e_w = w_ref_n - w
+        self.e_w_n = atan2(sin(e_w), cos(e_w))
+        return e_v, e_w
 
-        self.e_v_nm1 = self.e_v_n
-        self.e_w_nm1 = self.e_w_n
+    def compute_w_pid_factors(self):
+        a_w = self.K_P_W + self.K_I_W * DELTA_T / 2 + self.K_D_W / DELTA_T
+        b_w = - self.K_P_W + self.K_I_W * DELTA_T / 2 - 2 * self.K_D_W / DELTA_T
+        c_w = self.K_D_W / DELTA_T
+        return a_w, b_w, c_w
+
+    def compute_v_pid_factors(self):
+        a_v = self.K_P_V + self.K_I_V * DELTA_T / 2 + self.K_D_V / DELTA_T
+        b_v = -self.K_P_V + self.K_I_V * DELTA_T / 2 - 2 * self.K_D_V / DELTA_T
+        c_v = self.K_D_V / DELTA_T
+        return a_v, b_v, c_v
 
     def limit_angular_speed_reference(self, w_ref_n):
         if w_ref_n > self.MAX_W:
