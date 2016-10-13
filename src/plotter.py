8b447045f1eee9e11405761b6f02146a15f37dde
m <lmiguelvargasf@gmail.com>
2016-10-13 11:09:55 -0500

    Plot followed trajectory vs reference

--- src/plotter.py
+++ src/plotter.py
@@ -47,9 +47,12 @@ class Plotter:
         self.plots_part_0[1, 1].plot(self.t, zeros, 'r--', label='e=0', lw=2)
         self.plots_part_0[1, 1].plot(self.t, e_y, 'b', label='y error')
 
+        self.plots_part_1[0].plot(self.x_ref, self.y_ref, 'r--', label='ref', lw=2)
+        self.plots_part_1[0].plot(self.x, self.y, 'b', label='real')
         self.decorate_plot(self.plots_part_0[0, 0], 'x and x ref vs. t', 't[s]', 'x[m]')
         self.decorate_plot(self.plots_part_0[0, 1], 'x error vs. t', 't[s]', 'x-error[m]')
         self.decorate_plot(self.plots_part_0[1, 0], 'y and y ref vs. t', 't[s]', 'y[m]')
         self.decorate_plot(self.plots_part_0[1, 1], 'y error vs. t', 't[s]', 'y-error[m]')
 
+        self.decorate_plot(self.plots_part_1[0], 'followed vs reference trajectory', 'x[m]', 'y[m]')
         plt.show()
