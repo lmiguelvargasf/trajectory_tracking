7e07f9527fe9a98771dc5604e146eab2035ddacc
m <lmiguelvargasf@gmail.com>
2016-10-13 01:27:19 -0500

    Define PID constants

--- src/constants.py
+++ src/constants.py
@@ -15,3 +15,12 @@ STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
 K_X = 0.90
 K_Y = 0.90
 K_THETA = 0.90
+
+# PID control constants
+K_P_V = 0.2
+K_I_V = 1.905
+K_D_V = 0.00
+
+K_P_W = 0.45
+K_I_W = 1.25
+K_D_W = 0.000
