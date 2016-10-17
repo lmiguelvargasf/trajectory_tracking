5c154a124affd8968f2f9f1588fa2241add8b41d
m <lmiguelvargasf@gmail.com>
2016-10-17 14:02:01 -0500

    Rename directory

--- src/constants.py
+++ src/constants.py
@@ -32,4 +32,4 @@ elif TRAJECTORY == 'squared':
 
 DELTA_T = 0.1 # this is the sampling time
 STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
-RESULTS_DIRECTORY = '../last_results/'
+RESULTS_DIRECTORY = '../txt_results/'
