1a6bcaaacc6813c017403018981fb34c5014f38a
m <lmiguelvargasf@gmail.com>
2016-10-19 19:52:17 -0500

    Add constants from where to import data

--- src/constants.py
+++ src/constants.py
@@ -36,3 +36,5 @@ STEPS = int(SIMULATION_TIME_IN_SECONDS / DELTA_T)
 # directory from where data is imported to or exported from
 RESULTS_DIRECTORY = '../txt_results/'
 PATH_TO_EXPORT_DATA = RESULTS_DIRECTORY + CONTROLLER + '/' + TRAJECTORY + '/'
+PATH_TO_IMPORT_EULER_DATA = RESULTS_DIRECTORY + 'euler' + '/' + TRAJECTORY + '/'
+PATH_TO_IMPORT_PID_DATA = RESULTS_DIRECTORY + 'pid' + '/' + TRAJECTORY + '/'
