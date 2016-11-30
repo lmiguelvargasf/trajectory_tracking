a510850c9569831d2827d41da65b1edb1e7ce37e
m <lmiguelvargasf@gmail.com>
2016-11-29 21:00:27 -0500

    test: Test trajectory class and subclasses

--- src/test/test_trajectory.py
+++ src/test/test_trajectory.py
@@ -3,13 +3,20 @@ import unittest
 
 from geometry_msgs.msg import Point
 
-from trajectory import LinearTrajectory, NegativeTimeException
+from trajectory import LinearTrajectory, NegativeTimeException, CircularTrajectory, Trajectory, SquaredTrajectory
+
+
+class TrajectoryTest(unittest.TestCase):
+
+    def test_when_time_is_negative_then_an_exception_is_raised(self):
+        trajectory = Trajectory()
+        self.assertRaises(NegativeTimeException, trajectory.get_position_at, -1)
+
 
 
 class LinearTrajectoryTest(unittest.TestCase):
 
     def setUp(self):
-        super(LinearTrajectoryTest, self).setUp()
         self.trajectory = LinearTrajectory(1, 0, 2, 0)
         self.expected_position = Point()
 
@@ -25,5 +32,45 @@ class LinearTrajectoryTest(unittest.TestCase):
         self.expected_position.y = 4
         self.assertEqual(self.expected_position, self.trajectory.get_position_at(1))
 
-    def test_given_trajectory_when_time_is_negative_an_exception_is_raised(self):
-        self.assertRaises(NegativeTimeException, self.trajectory.get_position_at, -1)
+
+class CircularTrajectoryTest(unittest.TestCase):
+
+    def setUp(self):
+        self.trajectory = CircularTrajectory(5, 4)
+        self.expected_position = Point()
+
+    def test_given_circular_trajectory_centered_at_origin_when_getting_position_then_position_is_returned(self):
+        self.expected_position.x = 5
+        self.expected_position.y = 0
+        self.trajectory = CircularTrajectory(5, 4)
+        self.assertPositionAlmostEqual(self.expected_position, self.trajectory.get_position_at(1), 0.01)
+
+    def test_given_circular_trajectory_not_centered_at_origin_when_getting_position_then_position_is_returned(self):
+        self.trajectory.x_0 = 5
+        self.trajectory.y_0 = 5
+        self.expected_position.x = 10
+        self.expected_position.y = 5
+        self.assertPositionAlmostEqual(self.expected_position, self.trajectory.get_position_at(1), 0.01)
+
+    def assertPositionAlmostEqual(self, expected, actual, delta):
+        self.assertAlmostEqual(expected.x, actual.x, delta=delta)
+        self.assertAlmostEqual(expected.y, actual.y, delta=delta)
+
+
+class SquaredTrajectoryTest(unittest.TestCase):
+
+    def setUp(self):
+        self.trajectory = SquaredTrajectory(3, 4)
+        self.expected_position = Point()
+
+    def test_given_squared_trajectory_with_corner_at_the_origin_when_getting_position_then_position_is_returned(self):
+        self.expected_position.x = 3
+        self.expected_position.y = 3
+        self.assertEqual(self.expected_position, self.trajectory.get_position_at(2))
+
+    def test_given_squared_trajectory_with_corner_not_at_the_origin_when_getting_position_then_position_is_returned(self):
+        self.trajectory.x_0 = 1
+        self.trajectory.y_0 = 1
+        self.expected_position.x = 4
+        self.expected_position.y = 4
+        self.assertEqual(self.expected_position, self.trajectory.get_position_at(2))
