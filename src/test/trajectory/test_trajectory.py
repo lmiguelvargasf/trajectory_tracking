c62ddc759908950c73437416dbd089903c0a6d5b
m <lmiguelvargasf@gmail.com>
2016-12-01 11:30:36 -0500

    refactor: Create package for trajectory tests
    Create a package trajectory inside tests.

--- src/test/trajectory/test_trajectory.py
+++ src/test/trajectory/test_trajectory.py
@@ -6,7 +6,6 @@ from geometry_msgs.msg import Point
 from trajectory.circular_trajectory import CircularTrajectory
 from trajectory.squared_trajectory import SquaredTrajectory
 from trajectory.trajectory import NegativeTimeException, Trajectory
-from trajectory.linear_trajectory import LinearTrajectory
 
 
 class TrajectoryTest(unittest.TestCase):
@@ -14,26 +13,6 @@ class TrajectoryTest(unittest.TestCase):
     def test_when_time_is_negative_then_an_exception_is_raised(self):
         trajectory = Trajectory()
         self.assertRaises(NegativeTimeException, trajectory.get_position_at, -1)
-
-
-
-class LinearTrajectoryTest(unittest.TestCase):
-
-    def setUp(self):
-        self.trajectory = LinearTrajectory(1, 0, 2, 0)
-        self.expected_position = Point()
-
-    def test_given_trajectory_at_origin_when_getting_position_after_1s_then_position_is_returned(self):
-        self.expected_position.x = 1
-        self.expected_position.y = 2
-        self.assertEqual(self.expected_position, self.trajectory.get_position_at(1))
-
-    def test_given_trajectory_not_at_origin_when_getting_position_after_1s_then_position_is_returned(self):
-        self.trajectory.x_0 = 1
-        self.trajectory.y_0 = 2
-        self.expected_position.x = 2
-        self.expected_position.y = 4
-        self.assertEqual(self.expected_position, self.trajectory.get_position_at(1))
 
 
 class CircularTrajectoryTest(unittest.TestCase):
