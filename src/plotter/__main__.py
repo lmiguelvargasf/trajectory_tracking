fea84dae4b279b5bfaea01c03d191d2df62f77c7
m <lmiguelvargasf@gmail.com>
2017-01-05 21:28:04 -0500

    refactor: Plot results of last simulation

--- src/plotter/__main__.py
+++ src/plotter/__main__.py
@@ -46,33 +46,28 @@ if __name__ == '__main__':
 
     cursor.execute(QUERIES['select_sim'])
 
-    if len(parameters) == 2 and parameters[1] == '--sims':
-        print('Simulations:')
+    if len(parameters) == 1:
+        print("Plotting results of the last simulation...")
+        simulation_name = cursor.fetchone()[0]
+        controller = str(simulation_name).split('_')[0]
+
+        cursor.execute(QUERIES['select_data'].format(simulation_name))
+        plot_data = PlotData()
+
         for row in cursor.fetchall():
-            print(row[0])
-        sys.exit(0)
+            plot_data.t.append(row[0])
+            plot_data.x.append(row[1])
+            plot_data.x_ref.append(row[2])
+            plot_data.y.append(row[3])
+            plot_data.y_ref.append(row[4])
+            plot_data.theta.append(row[5])
+            plot_data.theta_ref.append(row[6])
+            plot_data.v_c.append(row[7])
+            plot_data.w_c.append(row[8])
+            plot_data.zeros.append(0)
 
-    print("Plotting results of the last simulation...")
-    simulation_name = cursor.fetchone()[0]
-    controller = str(simulation_name).split('_')[0]
-
-    cursor.execute(QUERIES['select_data'].format(simulation_name))
-    plot_data = PlotData()
-
-    for row in cursor.fetchall():
-        plot_data.t.append(row[0])
-        plot_data.x.append(row[1])
-        plot_data.x_ref.append(row[2])
-        plot_data.y.append(row[3])
-        plot_data.y_ref.append(row[4])
-        plot_data.theta.append(row[5])
-        plot_data.theta_ref.append(row[6])
-        plot_data.v_c.append(row[7])
-        plot_data.w_c.append(row[8])
-        plot_data.zeros.append(0)
-
-    plotter = SimulationPlotter(plot_data, controller)
-    plotter.plot_results()
+        plotter = SimulationPlotter(plot_data, controller)
+        plotter.plot_results()
 
     cursor.close()
     connection.close()
