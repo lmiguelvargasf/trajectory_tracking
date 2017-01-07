6e059690fcee7135deea13b31bf3b5248e23b68b
m <lmiguelvargasf@gmail.com>
2017-01-06 21:28:40 -0500

    feat: Create database context manager

---
+++ src/db_context_manager.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sqlite3
+
+
+class DBContextManager:
+    def __init__(self, db_path):
+        self.db_path = db_path
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
