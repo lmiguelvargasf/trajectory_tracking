b20c3bb0e12fec8341cd16c9cf0773a58a6855db
m <lmiguelvargasf@gmail.com>
2017-01-07 20:51:02 -0500

    refactor: Rename figs and plots

--- src/plotter/comparison_plotter.py
+++ src/plotter/comparison_plotter.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 
 class ComparisonPlotter:
     def __init__(self, data_list):
-        self.fig_part_0, self.plots_part_0 = plt.subplots(1, 1)
-        self.fig_part_1, self.plots_part_1 = plt.subplots(2, 1, sharex=True)
-        self.fig_part_2, self.plots_part_2 = plt.subplots(2, 1, sharex=True)
-        self.fig_part_3, self.plots_part_3 = plt.subplots(2, 1, sharex=True)
+        self.trajectory_fig, self.trajectory_plot = plt.subplots(1, 1)
+        self.position_fig, self.position_plot = plt.subplots(2, 1, sharex=True)
+        self.position_error_fig, self.position_error_plot = plt.subplots(2, 1, sharex=True)
+        self.control_action_fig, self.control_action_plot = plt.subplots(2, 1, sharex=True)
 
     def plot_comparison(self):
-        pass
+        plt.show()
+
+if __name__ == '__main__':
+    ComparisonPlotter([]).plot_comparison()
