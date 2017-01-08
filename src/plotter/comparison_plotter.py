5fb1b00c031bacc99d754a0e683c59bf9b1ea1aa
m <lmiguelvargasf@gmail.com>
2017-01-07 21:00:55 -0500

    feat: Extract relevant data

--- src/plotter/comparison_plotter.py
+++ src/plotter/comparison_plotter.py
@@ -9,6 +9,12 @@ class ComparisonPlotter:
         self.position_error_fig, self.position_error_plot = plt.subplots(2, 1, sharex=True)
         self.control_action_fig, self.control_action_plot = plt.subplots(2, 1, sharex=True)
 
+        temp_data = data_list[0]
+        self.t = temp_data['t']
+        self.x_ref = temp_data['x_ref']
+        self.y_ref = temp_data['y_ref']
+        self.zeros = temp_data['zeros']
+
     def plot_comparison(self):
         plt.show()
 
