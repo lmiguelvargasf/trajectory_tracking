f8c4ec649eb67614fd3e3bd9535eb3536421a36d
m <lmiguelvargasf@gmail.com>
2017-01-07 20:47:07 -0500

    feat: Create plots

--- src/plotter/comparison_plotter.py
+++ src/plotter/comparison_plotter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-from plotter.plotter import Plotter
+import matplotlib.pyplot as plt
 
 
 class ComparisonPlotter:
     def __init__(self, data_list):
-        pass
+        self.fig_part_0, self.plots_part_0 = plt.subplots(1, 1)
+        self.fig_part_1, self.plots_part_1 = plt.subplots(2, 1, sharex=True)
+        self.fig_part_2, self.plots_part_2 = plt.subplots(2, 1, sharex=True)
+        self.fig_part_3, self.plots_part_3 = plt.subplots(2, 1, sharex=True)
 
     def plot_comparison(self):
         pass
