3de1cdba6c438a5bc52c10fa469b675117b9ce45
m <lmiguelvargasf@gmail.com>
2017-01-11 00:27:19 -0500

    fix: Fix wrong formula for y position

--- src/trajectory/lissajous_trajectory.py
+++ src/trajectory/lissajous_trajectory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from math import pi, sin, cos
+from math import pi, sin
 
 from .trajectory import Trajectory
 
@@ -17,7 +17,7 @@ class LissajousTrajectory(object, Trajectory):
     def get_position_at(self, t):
         super(LissajousTrajectory, self).get_position_at(t)
         self.position.x = self.A * sin(2 * pi * t * self.a / self.period + self.delta)
-        self.position.y = -self.B * sin(2 * pi * t * self.b / self.period)
+        self.position.y = self.B * sin(2 * pi * t * self.b / self.period)
 
         return self.position
 
