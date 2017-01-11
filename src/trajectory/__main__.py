69fc35f074699f5229e7100cd6497db230378378
m <lmiguelvargasf@gmail.com>
2017-01-11 00:01:05 -0500

    feat: Plot trajectory using module

--- src/trajectory/__main__.py
+++ src/trajectory/__main__.py
@@ -2,11 +2,29 @@
 import os
 import sys
 
+import matplotlib.pylab as plt
+
+from .builder import create_trajectory
 from .util import print_error_message, print_usage
 from .util import print_files_in_path, get_trajectories_in_path
 
-def plot_trajectory():
-    pass
+
+def plot_trajectory(name):
+    STEPS = 600
+    DELTA = 1 if name != 'linear' else 0.1
+    trajectory = create_trajectory(name, STEPS)
+
+    x = [trajectory.get_position_at(i * DELTA).x for i in range(STEPS)]
+    y = [trajectory.get_position_at(i * DELTA).y for i in range(STEPS)]
+
+    trajectory_fig, trajectory_plot = plt.subplots(1, 1)
+    trajectory_plot.plot(x, y, label='trajectory', lw=3)
+    trajectory_plot.set_title(name.title() + ' Trajectory', fontsize=20)
+    trajectory_plot.set_xlabel(r'$x{\rm[m]}$', fontsize=18)
+    trajectory_plot.set_ylabel(r'$y{\rm[m]}$', fontsize=18)
+    trajectory_plot.legend(loc=0)
+    trajectory_plot.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -30,4 +48,4 @@ if __name__ == '__main__':
         print('Error: trajectory does not exist!')
         sys.exit(2)
 
-    plot_trajectory()
+    plot_trajectory(arg)
