c0165beccf1d53f8963d78ba2f2a6a81939f428b
m <lmiguelvargasf@gmail.com>
2017-01-10 19:56:05 -0500

    refactor: simplify controller

--- src/controller/euler_controller.py
+++ src/controller/euler_controller.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-from math import atan2, sin, pi
+from math import atan2, sin
 from math import cos
 
-from util.angle import get_angle_between_0_and_2_pi
 from .controller import Controller
 
 
@@ -15,7 +14,8 @@ class EulerMethodController(Controller):
         self.theta_ez_n_minus_1 = 0
         self.theta_n_minus_1 = 0
 
-    def set_next_reference(self, reference):
+    def set_next_reference(self):
+        reference = self.trajectory.get_position_at((self.i + 1) * self.delta)
         self.x_ref_n_plus_1 = reference.x
         self.y_ref_n_plus_1 = reference.y
 
@@ -26,24 +26,7 @@ class EulerMethodController(Controller):
         return self.y_ref_n_plus_1 - self.K_Y * (self.y_ref_n - self.y_n) - self.y_n
 
     def get_delta_theta_n(self):
-        self.theta_ez_n = get_angle_between_0_and_2_pi(self.theta_ez_n)
-        self.theta_n = get_angle_between_0_and_2_pi(self.theta_n)
-        self.theta_ez_n_minus_1 = get_angle_between_0_and_2_pi(self.theta_ez_n_minus_1)
-
-        epsilon = 4 * self.delta
-
-        alpha = self.theta_ez_n - self.K_THETA * (self.theta_ez_n_minus_1 - self.theta_n) - self.theta_n
-
-        if alpha == 2 * pi:
-            alpha = 0
-
-        if self.trajectory.get_name() == 'astroid' and (
-                            abs(self.simulation_time / 4 - self.i * self.delta) < epsilon or \
-                                abs(self.simulation_time / 2 - self.i * self.delta) < epsilon or \
-                            abs(3 * self.simulation_time / 4 - self.i * self.delta) < epsilon):
-            alpha = abs(alpha) * 2
-
-        return alpha
+        return self.theta_ez_n - self.K_THETA * (self.theta_ez_n_minus_1 - self.theta_n) - self.theta_n
 
     def compute_theta_ez_n(self):
          return atan2(self.get_delta_y_n(), self.get_delta_x_n())
@@ -61,11 +44,6 @@ class EulerMethodController(Controller):
         self.theta_n_minus_1 = self.theta_n
         self.theta_ref_n = self.theta_ez_n
 
-        if self.trajectory.get_name() in ('circular', 'squared', 'astroid'):
-            if not 0 <= self.i * self.delta < self.simulation_time / 4 + 10 * self.delta:
-                self.theta_ref_n = get_angle_between_0_and_2_pi(self.theta_ez_n)
-                self.theta_n = get_angle_between_0_and_2_pi(self.theta_n)
-
         # limit angular velocity to be between -pi and pi rad/s
         return atan2(sin(w_n), cos(w_n))
 
@@ -74,7 +52,7 @@ class EulerMethodController(Controller):
         self.set_current_orientation(pose.orientation)
         self.set_current_position(pose.position)
         self.set_current_reference(self.trajectory.get_position_at(i * self.delta))
-        self.set_next_reference(self.trajectory.get_position_at((i + 1) * self.delta))
+        self.set_next_reference()
 
         self.theta_ez_n = self.compute_theta_ez_n()
         self.v_c_n = self.compute_v_c_n()
