83c23e8d5c9af2243d5210d1f498e0b6aed196cf
m <lmiguelvargasf@gmail.com>
2017-01-25 20:58:53 -0500

    refactor: Rename variable

--- src/trajectory/builder.py
+++ src/trajectory/builder.py
@@ -6,16 +6,16 @@ from .lemniscate_trajectory import LemniscateTrajectory
 from .squared_trajectory import SquaredTrajectory
 from .linear_trajectory import LinearTrajectory
 
-def create_trajectory(trajectory_name, period):
-    if trajectory_name == 'linear':
+def create_trajectory(name, period):
+    if name == 'linear':
         return LinearTrajectory(0.05, 0.01, 0.05, 0.01)
-    elif trajectory_name == 'circular':
+    elif name == 'circular':
         return CircularTrajectory(2.0, period)
-    elif trajectory_name == 'squared':
+    elif name == 'squared':
         return SquaredTrajectory(2.0, period, 0.01, 0.01)
-    elif trajectory_name == 'lemniscate':
+    elif name == 'lemniscate':
         return LemniscateTrajectory(2.0, period)
-    elif trajectory_name == 'epitrochoid':
+    elif name == 'epitrochoid':
         return EpitrochoidTrajectory(5, 1, 3, period, 1 / 3.0)
-    elif trajectory_name == 'lissajous':
+    elif name == 'lissajous':
         return LissajousTrajectory(1, 1, 3, 2, period)
